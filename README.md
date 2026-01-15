9c8fddf0f5869703dd3e49d0edbf4fb9b6051721
RaphaelIT7 <64648134+RaphaelIT7@users.noreply.github.com>
2026-01-15 12:56:39 +0100

    repo: setup premake5

---
+++ README.md
@@ -0,0 +1,4 @@
+# gmod-vrmod
+This is a fork of the original gmod vrmod module<br>
+all of my changes can be tracked in the commit history, as the repository is based off the original files.<br>
+Original author / source is from https://catse.net/vrmod<br>
---
+++ development/module/premake5.lua
@@ -0,0 +1,12 @@
+-- Defines which version of the project generator to use, by default
+-- (can be overridden by the environment variable PROJECT_GENERATOR_VERSION)
+PROJECT_GENERATOR_VERSION = 3
+
+newoption({
+	trigger = "gmcommon",
+	description = "Sets the path to the garrysmod_common (https://github.com/danielga/garrysmod_common) directory",
+	default = io.open("../../../fork-garrysmod_common/license.txt", "r") and "../../../fork-garrysmod_common" or "../garrysmod_common"
+})
+
+MODULE_DEVELOPMENT = true
+load(io.readfile("../../premake5.lua"))()
